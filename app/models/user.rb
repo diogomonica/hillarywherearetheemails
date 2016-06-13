bbb3544de4978fd6be7bd471c12790ae780be809
Diogo Monica <diogo.monica@gmail.com>
2016-06-12 23:17:34 -0700

    Adding delay to individual tweets
    Signed-off-by: Diogo Monica <diogo.monica@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,20 +12,8 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.from_omniauth(auth)
-    puts "self.from_omniauth: #{auth.inspect}"
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_secret = auth.credentials.secret
-      user.save!
-    end
-  end
-
   def tweet(tweet, reply_to)
-    puts "tweet: oauth token: #{oauth_token}"
+    puts "about to tweet: #{tweet}"
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = Rails.application.secrets.omniauth_provider_key
       config.consumer_secret     = Rails.application.secrets.omniauth_provider_secret
