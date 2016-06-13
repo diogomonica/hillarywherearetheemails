409775c2cc3d3ea50efd98bd6b3d75cb4054138a
Diogo Monica <diogo.monica@gmail.com>
2016-06-13 00:02:06 -0700

    removing a bunch of stuff
    Signed-off-by: Diogo Monica <diogo.monica@gmail.com>

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,13 +6,8 @@ class TweetsController < ApplicationController
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def create
-    # current_user.tweet(twitter_params[:message])
-     current_user.tweet("testing my first twitter bot")
-  end
-
   def twitter_params
     params.require(:tweet).permit(:message)
   end
-     
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user?, :except => [:index]
-
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
 end
