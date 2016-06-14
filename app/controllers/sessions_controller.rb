57635377b6f28a4085274ca6c040ccdd2653ce4b
Diogo Monica <diogo.monica@gmail.com>
2016-06-13 18:14:35 -0700

    another attempt
    Signed-off-by: Diogo Monica <diogo.monica@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,9 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    user.oauth_token = auth.credentials.token
+    user.oauth_secret = auth.credentials.secret
+    user.save
     reset_session
     session[:user_id] = user.id
     session[:access_token] = user.oauth_token
